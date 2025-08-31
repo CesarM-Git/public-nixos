d94c291569ba51bcdb9dc603eb0aea368e473167
Cesar M <68117357+CesarM-Git@users.noreply.github.com>
2025-08-31 02:22:23 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,10 +3,8 @@ networking.hostName = "Nix-Home"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.wireless.networks = {
     "DNS" = {
-      #psk = "temppass123";
       pskRaw = "ext:wifi_temp_password";
       hidden = true;
       priority = 1;
-      #  extraConfig = "key_mgmt=SAE scan_ssid=1";
      };
   };
