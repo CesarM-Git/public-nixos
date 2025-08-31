e63531b8c0fbaf2c5c6eb286f28ca6dd16a251f7
Cesar M <68117357+CesarM-Git@users.noreply.github.com>
2025-08-31 02:22:35 -0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,5 +1,4 @@
 networking.hostName = "Nix-Home"; # Define your hostname.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.wireless.networks = {
     "DNS" = {
